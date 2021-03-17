df91bf9c70694f933d18765302d5f2e002d4b48d
Dennis Gustafsson <dennisgust@gmail.com>
2021-03-17 14:43:45 +0100

    Uppgift 1 Step 4

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -11,11 +11,25 @@ public class Calculator {
             return 0;
         }
 
+        if (hasDelimiter(numbers)) {
+            int index = numbers.indexOf("\\n");
+            String delimiter = numbers.substring(2, index);
+            numbers = numbers.substring(index + 2);
+            numbers = numbers.replace(delimiter, ",");
+        }
+
         String[] numberArr = numbers.replace("\\n", ",").split(",");
         int ans = 0;
         for (String str : numberArr) {
             ans = ans + Integer.parseInt(str);
         }
         return ans;
+    }
+
+    private boolean hasDelimiter(String str) {
+        if (str.length() < 4) return false;
+        if (!str.substring(0, 2).equals("//")) return false;
+        if (!str.contains("\\n")) return false;
+        return true;
     }
 }
